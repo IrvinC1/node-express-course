70f0dae231024e7a0fea4512e717b4d5ae924449
Irvin Castrejon <icastrejon@unitedvirtualities.com>
2021-08-11 14:01:14 -0500

    add first GET route

--- server.js
+++ server.js
@@ -3,6 +3,19 @@ const express = require('express');
 //creates an instance of the express constructor, which we will name "app".
 const app = express();
 
+const mockUserData = [
+    { name: 'Mark' },
+    { name: 'Jill' }
+]
+
+app.get('/users', function(req, res) {
+    res.json({
+        success: true,
+        message: 'successfully got users. Nice!',
+        users: mockUserData
+    })
+})
+
 app.listen(8000, function() {
     console.log("server is running")
 })
